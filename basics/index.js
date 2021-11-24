b8f395ee426ab8fef342b1529315ca8b13ffe0e0
balmadselena <balmadselena@gmail.com>
2021-11-24 12:40:31 +0800

    exam itgel

--- basics/index.js
+++ basics/index.js
@@ -1,9 +1,19 @@
 // 1. 0 - ээс 100 хүртэлх тоог хэвлэ.
 // -- Энд кодоо бичнэ үү!!
+for (let i = 0; i < 101; i++) {
+    console.log(i);
+}
+
 
 
 // 2. 60 - с 30 хүртэлх бүх СОНДГОЙ тоог хэвлэ.
 // -- Энд кодоо бичнэ үү!!
+for (let i = 60; i <= 30; i--) {
+    if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+
 
 // 3. Чагтаас бүрдсэн квадрат зурах ба баруун тийшээ налсан диагональ нь нэмэх тэмдэг байна:
 // ######+
@@ -14,6 +24,12 @@
 // #+#####
 // +######
 // -- Энд кодоо бичнэ үү!!
+function third() {
+    let string = "#".repeat(7)
+    for (var i = 6; i >= 0; i--) {
+        console.log(string.substring(0, i) + '+' + string.substring(i + 1));
+    }
+}
 
 // 4. Регистрийн дугаар өгөхөд төрсөн он, сар, өдөр (YYYY-MM-DD) хүйсийг хэвлэдэг convertRegNum функц бич.
 //      РД - гийн сүүлийн орон сондгой бол эрэгтэй, тэгш бол эмэгтэй байна.
@@ -21,19 +37,61 @@
 //      convertRegNum('АБ90020321')
 //      >> 1990-02-03 онд төрсөн эрэгтэй
 // -- Энд кодоо бичнэ үү!!
+function convertRegNum(reg) {
+    let string = ''
+
+    string += `19${reg.substring(2, 4)}-${reg.substring(4, 6)}-${reg.substring(6, 8)} онд төрсөн `
+
+    if (reg.substring(8, 10) % 2 == 1) {
+        string += 'эрэгтэй'
+    }
+    else {
+        string += 'эмэгтэй'
+    }
+    console.log(string);
+}
 
 // 5. Бүх .red-boxes гэсэн класстай элемэнтийг улаан болго;
 // -- Энд кодоо бичнэ үү!!
+let redBoxes = document.getElementsByClassName('red-boxes')
+
+for (let i = 0; i < redBoxes.length; i++) {
+    redBoxes[i].style.backgroundColor = 'red'
+}
 
 // 6. blue-box гэсэн ID-тай элемэнтийг цэнхэр болго;
 // -- Энд кодоо бичнэ үү!!
+let blueBoxes = document.getElementById('blue-box box')
+blueBoxes.style.backgroundColor = 'blue'
+
+
+
 
 // 7. Бүх div - ийг бор болго;
 // -- Энд кодоо бичнэ үү!!
+let boxes = document.getElementsByTagName('div')
 
-// 8. Шинээр box гэсэн класстай section tag үүсгээд, хар өнгөтэй болгоно.
-//    Дотор нь box гэсэн класстай article tag үүсгэнэ.
-//    article tag - ыг улаан өнгөтэй бөөрөнхий болгоно (JS - ээр)
-//    section tag - ыг body руу append хийнэ.
-//    улаан бөөрөнхий дээр дарахад бөөрөнхийг ногоон болгоно.
-// -- Энд кодоо бичнэ үү!!
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].style.backgroundColor = '#795548'
+
+    // 8. Шинээр box гэсэн класстай section tag үүсгээд, хар өнгөтэй болгоно.
+    //    Дотор нь box гэсэн класстай article tag үүсгэнэ.
+    //    article tag - ыг улаан өнгөтэй бөөрөнхий болгоно (JS - ээр)
+    //    section tag - ыг body руу append хийнэ.
+    //    улаан бөөрөнхий дээр дарахад бөөрөнхийг ногоон болгоно.
+    // -- Энд кодоо бичнэ үү!!
+    let section = document.createElement("section")
+    section.classList.add('box')
+    section.style.backgroundColor = 'black'
+
+    let article = document.createElement('article')
+    article.classList.add('box')
+    article.style.cssText = 'background-color: red; border-radius: 50%'
+    article.onclick = function () {
+        let tmp = document.getElementsByTagName('article')
+        tmp[0].style.cssText = 'background-color: green; border-radius: 50%'
+    }
+
+    section.appendChild(article)
+
+    document.body.appendChild(section)
--- datatypes/index.js
+++ datatypes/index.js
@@ -2,6 +2,8 @@ const string = 'apple, banana, kiwi';
 
 // 1. Өгсөн string - ийн бүх үсгийг том болго: 'APPLE, BANANA, KIWI'
 // -- Энд кодоо бичнэ үү!
+console.log(sentence.toUpperCase());
+
 
 // 2. Өгсөн string - ийг дараах хэлбэртэй болго: 'Apple; Banana; Kiwi'
 // -- Энд кодоо бичнэ үү!
@@ -9,22 +11,46 @@ const string = 'apple, banana, kiwi';
 const array = ['d', 'e', 'c', 'a', 'b'];
 // 3. Өгсөн array - ыг үсгийн дарааллаар эрэмблэ.
 // -- Энд кодоо бичнэ үү!
+const array = ['d', 'e', 'c', 'a', 'b'];
+array.sort
 
 // 4. Өгсөн array - ын голын 3н элемэнтийг салгаж аваад array1 - гэсэн хувьсагчид хийнэ.
 //    array болон array1 ийг хэвлэ.
 // -- Энд кодоо бичнэ үү!
+const array = ['d', 'e', 'c', 'a', 'b'];
+const array1 = array.slice(1,3);
+array = array.splice(1,3);
+console.log(array1);
+console.log(array);
 
 // 5. Өгсөн array - ын эхний элемэнтийг устага
 // -- Энд кодоо бичнэ үү!
+const array = ['d', 'e', 'c', 'a', 'b'];
+array.shift();
+console.log(array);
 
 // 6. Өгсөн array - ын сүүлийн элемэнтийг устага
 // -- Энд кодоо бичнэ үү!
-
+const array = ['d', 'e', 'c', 'a', 'b'];
+array.pop();
+console.log(array);
 // 7. Өгсөн array - ын урд 'z' үсэг нэм
 // -- Энд кодоо бичнэ үү!
+const array = ['d', 'e', 'c', 'a', 'b'];
+
+const newFirstElement = 'z'
+
+const newArray = [newFirstElement].concat(array)
+
+
 
 // 8. Өгсөн array - ын ард 'j' үсэг нэм
 // -- Энд кодоо бичнэ үү!
+const array = ['d', 'e', 'c', 'a', 'b'];
+
+array.push('j');
+
+console.log(array);
 
 // 9. Өгсөн array - ыг JSON string болго.
 // -- Энд кодоо бичнэ үү!
